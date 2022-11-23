14bad2ccd0ad17a47f62649ea8697e0112610b78
dltkd1395 <dltkd13956@naver.com>
2022-11-23 22:57:15 +0900

    feat(InputView) : 게임 재시작 여부 입력 기능 구현

--- src/main/kotlin/baseball/view/InputView.kt
+++ src/main/kotlin/baseball/view/InputView.kt
@@ -8,4 +8,9 @@ class InputView {
         println("숫자를 입력해주세요 : ")
         return Console.readLine()
     }
+
+    fun readGameCommand(): String {
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        return Console.readLine()
+    }
 }

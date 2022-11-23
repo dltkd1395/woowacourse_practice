a9e95cfec2c6263615f7179ccf74707bd98bcf58
dltkd1395 <dltkd13956@naver.com>
2022-11-24 00:37:04 +0900

    fix(InputView) : 숫자 입력 들여쓰기 수정

--- src/main/kotlin/baseball/view/InputView.kt
+++ src/main/kotlin/baseball/view/InputView.kt
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console
 class InputView {
     fun readBaseBallNumber(): String {
         println("숫자 야구 게임을 시작합니다.")
-        println("숫자를 입력해주세요 : ")
+        print("숫자를 입력해주세요 : ")
         return Console.readLine()
     }
 

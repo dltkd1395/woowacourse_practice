fbe2b68be4fe753c41a93454343b336c5bbf16d4
dltkd1395 <dltkd13956@naver.com>
2022-11-24 00:56:28 +0900

    fix(InputView) : 숫자 게임 시작 출력 제거

--- src/main/kotlin/baseball/view/InputView.kt
+++ src/main/kotlin/baseball/view/InputView.kt
@@ -4,7 +4,6 @@ import camp.nextstep.edu.missionutils.Console
 
 class InputView {
     fun readBaseBallNumber(): String {
-        println("숫자 야구 게임을 시작합니다.")
         print("숫자를 입력해주세요 : ")
         return Console.readLine()
     }

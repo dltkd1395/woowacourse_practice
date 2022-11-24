fbb11120ef8c0e6846258e8ab8492bb60fff489e
dltkd1395 <dltkd13956@naver.com>
2022-11-25 01:34:17 +0900

    fix(LottoGenerator) : 로또 살 수 있는 개수 오류 수정

--- src/main/kotlin/lotto/domain/LottoGenerator.kt
+++ src/main/kotlin/lotto/domain/LottoGenerator.kt
@@ -4,15 +4,16 @@ import camp.nextstep.edu.missionutils.Randoms
 import lotto.utils.LOTTO_MAX_NUMBER
 import lotto.utils.LOTTO_MIN_NUMBER
 import lotto.utils.MAX_LENGTH
+import lotto.utils.THOUSAND
 
-class LottoGenerator(private val count: Int) {
+class LottoGenerator(private val money: Int) {
     fun generate(): List<List<Int>> {
         val lottos = mutableListOf<List<Int>>()
+        val count = money / THOUSAND
         while (lottos.size < count) {
             val randomNumbers = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, MAX_LENGTH)
             lottos.add(randomNumbers)
         }
         return lottos
     }
-
 }

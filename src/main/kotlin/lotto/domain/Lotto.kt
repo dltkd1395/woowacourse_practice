b2b53d4b2c0bbf1864507029ccc4f48fc88ac97c
dltkd1395 <dltkd13956@naver.com>
2022-11-24 20:37:17 +0900

    refactor(Lotto) : 중복되는 예외처리 수정

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,25 +1,21 @@
 package lotto.domain
 
 import lotto.utils.ErrorType
-import lotto.utils.LOTTO_MAX_NUMBER
-import lotto.utils.LOTTO_MIN_NUMBER
+import lotto.utils.validations.Validator
 
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { ErrorType.LOTTO_SIZE.errorMessage }
         require(isDuplication()) { ErrorType.LOTTO_DUPLICATION.errorMessage }
-        require(isNumberRange()) { ErrorType.LOTTO_RANGE.errorMessage }
+        isNumberRange()
     }
 
     private fun isDuplication(): Boolean =
         numbers.size == numbers.distinct().size
 
-    private fun isNumberRange(): Boolean {
+    private fun isNumberRange() {
         numbers.map { number ->
-            if (number !in LOTTO_MIN_NUMBER..LOTTO_MAX_NUMBER) {
-                return false
-            }
+            Validator.validateNumberRange(number)
         }
-        return true
     }
 }

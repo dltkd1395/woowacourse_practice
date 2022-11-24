8793864d5c7e2d2e78f1d2271055185220da93cf
dltkd1395 <dltkd13956@naver.com>
2022-11-25 02:47:26 +0900

    fix(JudgementTest) : Judgement 클래스 List 타입 변경으로 인한 오류 수정

--- src/test/kotlin/lotto/JudgementTest.kt
+++ src/test/kotlin/lotto/JudgementTest.kt
@@ -2,14 +2,13 @@ package lotto
 
 import lotto.domain.Judgement
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
 class JudgementTest {
     @ParameterizedTest
-    @CsvSource("1,1,true", "2,2,true", "3,4,false")
-    fun `로또 번호와 당첨 번호를 비교한다`(lottoNumber: Int, winningNumber: Int, expected: Boolean) {
+    @CsvSource("1,[1,2,3,4,5,6],true", "2,[1,2,3,4,5,6],true", "8,[1,2,3,4,5,6],false")
+    fun `로또 번호와 당첨 번호를 비교한다`(lottoNumber: Int, winningNumber: List<Int>, expected: Boolean) {
         val actual = Judgement(lottoNumber, winningNumber).isLottoMatching()
         assertThat(actual).isEqualTo(expected)
     }

3e6692cf5f13f61d85073d8b39a571a6dfaa6406
dltkd1395 <dltkd13956@naver.com>
2022-11-26 21:57:09 +0900

    feat(InputView) : 다리 건너기 게임 입력 기능 구현

--- src/main/kotlin/bridge/view/InputView.kt
+++ src/main/kotlin/bridge/view/InputView.kt
@@ -1,5 +1,10 @@
 package bridge.view
 
+import bridge.uilts.INPUT_BRIDGE_MOVEMENT
+import bridge.uilts.INPUT_BRIDGE_RETRY
+import bridge.uilts.INPUT_BRIDGE_SIZE
+import camp.nextstep.edu.missionutils.Console
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -7,21 +12,24 @@ class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    fun readBridgeSize(): Int {
-        return 0
+    fun readBridgeSize(): String {
+        println(INPUT_BRIDGE_SIZE)
+        return Console.readLine()
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        return ""
+        println(INPUT_BRIDGE_MOVEMENT)
+        return Console.readLine()
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        return ""
+        println(INPUT_BRIDGE_RETRY)
+        return Console.readLine()
     }
 }

3f39e8d6d2881cca8027660350d7225e203b7b7a
dltkd1395 <dltkd13956@naver.com>
2022-11-27 00:12:25 +0900

    feat(BridgeGame) : 게임 재시작하는 기능 구현

--- src/main/kotlin/bridge/domain/BridgeGame.kt
+++ src/main/kotlin/bridge/domain/BridgeGame.kt
@@ -26,7 +26,10 @@ class BridgeGame {
      *
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun retry() {}
+    fun retry(player: Player, bridge: Bridge) {
+        player.retryCount++
+        bridge.resetBridge()
+    }
 
     private fun setBridgeResult(playerDirection: String, result: String, bridge: Bridge) {
         when (playerDirection) {

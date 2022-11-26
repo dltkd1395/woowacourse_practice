b41bb04a6cec9a451b61ce43adf3737c10e92066
dltkd1395 <dltkd13956@naver.com>
2022-11-27 00:10:07 +0900

    feat(BridgeGame) : 다리 움직이는 기능 구현

--- src/main/kotlin/bridge/domain/BridgeGame.kt
+++ src/main/kotlin/bridge/domain/BridgeGame.kt
@@ -1,5 +1,10 @@
 package bridge.domain
 
+import bridge.uilts.DOWN
+import bridge.uilts.NOT_CROSS
+import bridge.uilts.UP
+import bridge.uilts.ZERO
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -10,7 +15,10 @@ class BridgeGame {
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move() {}
+    fun move(direction: String, result: String, bridge: Bridge) {
+        setBridgeResult(direction, result, bridge)
+        bridge.size++
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -19,4 +27,28 @@ class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun retry() {}
+
+    private fun setBridgeResult(playerDirection: String, result: String, bridge: Bridge) {
+        when (playerDirection) {
+            UP -> setUpBridge(result, bridge)
+            DOWN -> setDownBridge(result, bridge)
+        }
+    }
+
+    private fun setUpBridge(result: String, bridge: Bridge) {
+        bridge.up += setBridge(result, bridge)
+        bridge.down += setBridge(NOT_CROSS, bridge)
+    }
+
+    private fun setDownBridge(result: String, bridge: Bridge) {
+        bridge.down += setBridge(result, bridge)
+        bridge.up += setBridge(NOT_CROSS, bridge)
+    }
+
+    private fun setBridge(result: String, bridge: Bridge): String {
+        if (bridge.size == ZERO) {
+            return " ${result} "
+        }
+        return "| ${result} "
+    }
 }

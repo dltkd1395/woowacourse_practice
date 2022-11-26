8e405f22d4013339653108cdc6ae7c2fac9e54ef
dltkd1395 <dltkd13956@naver.com>
2022-11-26 23:18:36 +0900

    chore(Constants) : 파일 이름 수정

--- src/main/kotlin/bridge/uilts/Constants.kt
+++ src/main/kotlin/bridge/uilts/Constants.kt
@@ -1,6 +1,7 @@
 package bridge.uilts
 
 const val ZERO = 0
+const val ONE = 1
 const val COMMAND_LENGTH = 1
 
 const val MIN_SIZE = 3
@@ -9,12 +10,14 @@ const val MAX_SIZE = 20
 const val UP = "U"
 const val DOWN = "D"
 
+const val EMPTY = ""
+
 const val RETRY = "R"
 const val QUIT = "Q"
 
 const val SUCCESS = "O"
 const val FAILURE = "X"
 
-const val INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요."
+const val INPUT_BRIDGE_SIZE = "이동할 칸을 선택해주세요."
 const val INPUT_BRIDGE_MOVEMENT = "이동할 칸을 선택해주세요."
-const val INPUT_BRIDGE_RETRY = "게임을 다시 시도할지 여부를 입력해주세요."
+const val INPUT_BRIDGE_RETRY = "게임을 다시 시도할지 여부를 입력해주세요"

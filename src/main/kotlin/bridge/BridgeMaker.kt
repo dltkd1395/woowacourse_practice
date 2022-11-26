a484df958fd4badb6b979def86f33eaaa3b95894
dltkd1395 <dltkd13956@naver.com>
2022-11-26 22:55:15 +0900

    feat(BridgeMaker) : 다리를 생성하는 기능 구현

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -1,5 +1,10 @@
 package bridge
 
+import bridge.uilts.DOWN
+import bridge.uilts.ONE
+import bridge.uilts.UP
+import bridge.uilts.ZERO
+
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -9,6 +14,18 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
-        return listOf()
+        val bridge = mutableListOf<String>()
+        while (bridge.size < size) {
+            val direction = getDirection(bridgeNumberGenerator.generate())
+            bridge.add(direction)
+        }
+        return bridge
+    }
+
+    private fun getDirection(value: Int): String {
+        if (value == ZERO) {
+            return DOWN
+        }
+        return UP
     }
 }

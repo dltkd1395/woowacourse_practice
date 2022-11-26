933080a8788691a6a1340e163b025d76a3ac2824
dltkd1395 <dltkd13956@naver.com>
2022-11-27 00:23:12 +0900

    feat(OutputView) : 다리 건너기 출력 기능 구현

--- src/main/kotlin/bridge/uilts/Constants.kt
+++ src/main/kotlin/bridge/uilts/Constants.kt
@@ -20,6 +20,14 @@ const val FAILURE = "X"
 
 const val NOT_CROSS = " "
 
+const val GAME_SUCCESS = "성공"
+const val GAME_FAILURE = "실패"
+
 const val INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요."
 const val INPUT_BRIDGE_MOVEMENT = "이동할 칸을 선택해주세요. (위: U, 아래: D)"
 const val INPUT_BRIDGE_RETRY = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"
+
+const val OUTPUT_GAME_START = "다리 건너기 게임을 시작합니다."
+const val OUTPUT_GAME_SUCCESS = "게임 성공 여부: "
+const val OUTPUT_GAME_RETRY = "총 시도한 횟수: "
+const val OUTPUT_GAME_RESULT = "최종 게임 결과"
--- src/main/kotlin/bridge/view/OutputView.kt
+++ src/main/kotlin/bridge/view/OutputView.kt
@@ -1,20 +1,49 @@
 package bridge.view
 
+import bridge.domain.Bridge
+import bridge.domain.Player
+import bridge.uilts.*
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 class OutputView {
+
+    fun printGameStart() {
+        println(OUTPUT_GAME_START)
+    }
+
+    fun printGameSuccess(player: Player) {
+        when (player.isSuccess) {
+            true -> println(OUTPUT_GAME_SUCCESS + GAME_SUCCESS)
+            false -> println(OUTPUT_GAME_SUCCESS + GAME_FAILURE)
+        }
+    }
+
+    fun printGameRetry(player: Player) {
+        println(OUTPUT_GAME_RETRY + "${player.retryCount}")
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap() {}
+
+    fun printMap(bridge: Bridge) {
+        println("[${bridge.up}]")
+        println("[${bridge.down}]")
+        println()
+    }
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printResult() {}
+    fun printResult(bridge: Bridge) {
+        println(OUTPUT_GAME_RESULT)
+        printMap(bridge)
+        println()
+    }
 }

d01bc95be7c857e6b0d43eb9deb340f976d481c1
dltkd1395 <dltkd13956@naver.com>
2022-11-29 00:08:13 +0900

    feat(OutputView) : 자동차 경주 게임 출력 기능 구현

--- src/main/kotlin/racingcar/utils/Constants.kt
+++ src/main/kotlin/racingcar/utils/Constants.kt
@@ -11,3 +11,7 @@ const val THRESHOLD = 4
 
 const val INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
 const val INPUT_TRY_NUMBER = "시도할 회수는 몇회인가요?"
+
+const val OUTPUT_RESULT = "실행 결과"
+const val DISTANCE = "-"
+const val WINNER = "최종 우승자"

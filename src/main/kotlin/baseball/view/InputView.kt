fad992ca75325c850f346835a58b32ef7d6259d3
dltkd1395 <dltkd13956@naver.com>
2022-11-30 01:35:33 +0900

    refactor(InputView) : list 타입으로 수정

--- src/main/kotlin/baseball/view/InputView.kt
+++ src/main/kotlin/baseball/view/InputView.kt
@@ -1,11 +1,13 @@
 package baseball.view
 
+import baseball.util.ONE
 import camp.nextstep.edu.missionutils.Console
 
 class InputView {
-    fun readBaseBallNumber(): String {
+    fun readBaseBallNumber(): List<String> {
         print("숫자를 입력해주세요 : ")
-        return Console.readLine()
+        val numbers = Console.readLine().split("")
+        return numbers.subList(ONE, numbers.size - 1)
     }
 
     fun readGameCommand(): String {

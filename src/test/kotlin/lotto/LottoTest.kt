f52ebc4c11a7500640e84d8e65ae95699fd10a44
dltkd1395 <dltkd13956@naver.com>
2022-12-02 02:58:45 +0900

    refactor(LottoTest) : Lotto 일급 컬렉션으로 인한 수정

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import lotto.domain.Lotto
+import lotto.domain.LottoNumber
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -9,22 +10,25 @@ class LottoTest {
     @Test
     fun `로또 번호의 개수가 6개가 넘어가면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
+            val lottoNumber = listOf(1, 2, 3, 4, 5, 6, 7).map { LottoNumber(it) }
+            Lotto(lottoNumber)
         }
     }
 
     // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
+        val lottoNumber = listOf(1, 2, 3, 4, 5, 6, 7).map { LottoNumber(it) }
         assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 5))
+            Lotto(lottoNumber)
         }
     }
 
     @Test
     fun `로또 번호의 숫자가 1부터 45까지의 숫자가 아닌 경우 예외가 발생한다`() {
+        val lottoNumber = listOf(1, 2, 3, 4, 5, 6, 7).map { LottoNumber(it) }
         assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 77))
+            Lotto(lottoNumber)
         }
     }
 }

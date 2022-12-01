14996c5d80fd1ea0259c6b6f82b422945d8a0385
dltkd1395 <dltkd13956@naver.com>
2022-12-02 03:00:52 +0900

    refactor(InputView) : WinningNumber를 List로 변환

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -5,15 +5,15 @@ import lotto.utils.INPUT_BONUS_NUMBER
 import lotto.utils.INPUT_LOTTO_AMOUNT
 import lotto.utils.INPUT_WINNING_NUMBER
 
-class InputView {
+object InputView {
     fun readLottoAmount(): String {
         println(INPUT_LOTTO_AMOUNT)
         return Console.readLine()
     }
 
-    fun readWinningNumber(): String {
+    fun readWinningNumber(): List<String> {
         println(INPUT_WINNING_NUMBER)
-        return Console.readLine()
+        return Console.readLine().split(",")
     }
 
     fun readBonusNumber(): String {

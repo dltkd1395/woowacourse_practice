c2658c5ecd973ecb45856bf77b6bc17f4aec9125
dltkd1395 <dltkd13956@naver.com>
2022-12-02 03:00:02 +0900

    refactor(Lotto) :  List 변환 수정

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -10,7 +10,7 @@ data class Lotto(private val numbers: List<LottoNumber>) {
     }
 
     fun isMatchingBonus(bonus: LottoNumber): Boolean =
-        numbers.contains(bonus)
+        !numbers.contains(bonus)
 
     fun matchs(winningLotto: WinningLotto): MatchResult =
         MatchResult(
@@ -19,6 +19,8 @@ data class Lotto(private val numbers: List<LottoNumber>) {
             },
             isMatchingBonus(winningLotto.bonus)
         )
+
+    fun toList() = numbers.toList()
 
     private fun match(lotto: LottoNumber): Boolean =
         numbers.contains(lotto)

91434b60af5c7ec5e7929fb14af869a7c7c5cb94
dltkd1395 <dltkd13956@naver.com>
2022-12-05 01:58:11 +0900

    refactor(BridgeMaker) : 리턴 타입 생략

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -1,9 +1,8 @@
 package bridge
 
-import bridge.uilts.DOWN
-import bridge.uilts.ONE
-import bridge.uilts.UP
-import bridge.uilts.ZERO
+import bridge.utils.DOWN
+import bridge.utils.UP
+import bridge.utils.ZERO
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -13,14 +12,11 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
-    fun makeBridge(size: Int): List<String> {
-        val bridge = mutableListOf<String>()
-        while (bridge.size < size) {
-            val direction = getDirection(bridgeNumberGenerator.generate())
-            bridge.add(direction)
-        }
-        return bridge
-    }
+    fun makeBridge(size: Int): List<String> =
+        List(size) { getDirection(bridgeNumberGenerator.generate()) }
+            .map { direction ->
+                (direction)
+            }
 
     private fun getDirection(value: Int): String {
         if (value == ZERO) {

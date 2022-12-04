f892c1cd9ac5cb9474d5e1a709621ba28585a1df
dltkd1395 <dltkd13956@naver.com>
2022-12-05 02:01:59 +0900

    refactor(Application) : BridgeController의 run 메서드 이름 수정

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,13 +1,8 @@
 package bridge
 
 import bridge.controller.BridgeController
-import bridge.controller.InputController
-import bridge.domain.*
-import bridge.uilts.*
-import camp.nextstep.edu.missionutils.Console
-import org.mockito.invocation.InvocationContainer
 
 fun main() {
-    BridgeController().run()
+    BridgeController().start()
 }
 

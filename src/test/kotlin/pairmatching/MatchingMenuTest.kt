8ebaaac4c8fec3d6b19365906f2c91ee5bfda1f0
dltkd1395 <dltkd13956@naver.com>
2022-12-06 22:38:25 +0900

    chore(MatchingMenuTest) : 파일 이름 수정

--- src/test/kotlin/pairmatching/MatchingMenuTest.kt
+++ src/test/kotlin/pairmatching/MatchingMenuTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 import pairmatching.domain.MatchingMenu
 
-class MatchingMenu {
+class MatchingMenuTest {
     @ParameterizedTest
     @MethodSource("provideMenu")
     fun `매칭 메뉴가 3개를 입력하지 않은 경우 예외가 발생한다`(menu: List<String>) {

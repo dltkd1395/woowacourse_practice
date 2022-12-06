98cac77a2915076cac07edd434533792b9554989
dltkd1395 <dltkd13956@naver.com>
2022-12-06 22:32:07 +0900

    feat(InputView) : 매칭할 메뉴 입력 기능 구현

--- src/main/kotlin/pairmatching/utils/Constants.kt
+++ src/main/kotlin/pairmatching/utils/Constants.kt
@@ -5,8 +5,45 @@ const val PAIR_INQUIY_COMMAND = "2"
 const val PAIR_INITIALIZATION_COMMAND = "3"
 const val QUIT_COMMAND = "Q"
 
-const val INPUT_FEATURE_SELECTION = """기능을 선택하세요.
+const val THREE = 3
+
+const val INPUT_FEATURE_SELECTION =
+    """기능을 선택하세요.
 1. 페어 매칭
 2. 페어 조회
 3. 페어 초기화
 Q. 종료"""
+
+const val INPUT_PAIR_MATCHING =
+    """
+#############################################
+과정: 백엔드 | 프론트엔드
+미션:
+  - 레벨1: 자동차경주 | 로또 | 숫자야구게임
+  - 레벨2: 장바구니 | 결제 | 지하철노선도
+  - 레벨3: 
+  - 레벨4: 성능개선 | 배포
+  - 레벨5: 
+############################################
+과정, 레벨, 미션을 선택하세요.
+ex) 백엔드, 레벨1, 자동차경주"""
+
+const val BACKEND_COURCE = "백엔드"
+const val FRONTEND_COURCE = "프론트엔드"
+const val LEVEL_1 = "레벨1"
+const val LEVEL_2 = "레벨2"
+const val LEVEL_3 = "레벨3"
+const val LEVEL_4 = "레벨4"
+const val LEVEL_5 = "레벨5"
+const val RACING_MINSSION = "자동차경주"
+const val LOTTO_MINSSION = "로또"
+const val BASEBALL_MINSSION = "숫자야구게임"
+const val SHOPPING_BASKET_MINSSION = "장바구니"
+const val PAYMENT_MINSSION = "결제"
+const val SUBWAY_MAP_MINSSION = "지하철노선도"
+const val IMPROVEMENT_MINSSION = "성능개선"
+const val DISTRIBUTION_MINSSION = "배포"
+
+const val PATH = "src/main/resource/"
+const val FILE_BACKEND = "backend-crew.md"
+const val FILE_FRONTEND = "frontend-crew.md"

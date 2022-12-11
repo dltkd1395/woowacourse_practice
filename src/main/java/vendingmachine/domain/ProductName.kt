85293eb2d2b7835ec093a8cd7c688b67df70876d
dltkd1395 <dltkd13956@naver.com>
2022-12-11 13:54:11 +0900

    feat(ProductName) : 상품명이 1글자 이상이 아닌 경우 알 수 있는 기능 구현

--- src/main/java/vendingmachine/domain/ProductName.kt
+++ src/main/java/vendingmachine/domain/ProductName.kt
@@ -1,3 +1,13 @@
 package vendingmachine.domain
 
-data class ProductName(val name: String)
+import vendingmachine.utils.ErrorType
+import vendingmachine.utils.ZERO
+
+data class ProductName(val name: String) {
+    init {
+        require(isNameLength()) { ErrorType.PRODUCT_NAME_LENGTH.errorMessage }
+    }
+
+    private fun isNameLength(): Boolean =
+        name.length > ZERO
+}

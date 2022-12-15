06ea36b9a7fa3414fc55353a3ed138478bddf1e5
dltkd1395 <dltkd13956@naver.com>
2022-12-15 14:15:55 +0900

    feat(InputView) : 경로 기준 입력 기능 구현

--- src/main/kotlin/subway/view/InputView.kt
+++ src/main/kotlin/subway/view/InputView.kt
@@ -1,8 +1,10 @@
 package subway.view
 
 import camp.nextstep.edu.missionutils.Console
+import subway.domain.InquiryType
 import subway.domain.PathInquiry
 import subway.utils.INPUT_FEATURE
+import subway.utils.INPUT_INQUIRY_TYPE
 import subway.utils.INPUT_PATH_INQUIRY
 
 object InputView {
@@ -11,5 +13,11 @@ object InputView {
         println(INPUT_PATH_INQUIRY)
         println(INPUT_FEATURE)
         return PathInquiry(Console.readLine())
+    }
+
+    fun readInquiryType(): InquiryType {
+        println(INPUT_INQUIRY_TYPE)
+        println(INPUT_FEATURE)
+        return InquiryType(Console.readLine())
     }
 }

19aea70067ba383e556519f16b887038f0965d25
dltkd1395 <dltkd13956@naver.com>
2022-12-15 13:54:33 +0900

    feat(InquiryType) : 경로 기준을 선택하는 입력 기능 구현

--- src/main/kotlin/subway/utils/ErrorMessage.kt
+++ src/main/kotlin/subway/utils/ErrorMessage.kt
@@ -3,3 +3,4 @@ package subway.utils
 const val ERROR = "[ERROR] %s"
 
 const val ERROR_PATH_INQUIRY = "경로 조회 명령어를 잘못 입력하였습니다."
+const val ERROR_INQUIRY_TYPE = "경로 기준 명령어를 잘못 입력하였습니다."
--- src/main/kotlin/subway/utils/ErrorType.kt
+++ src/main/kotlin/subway/utils/ErrorType.kt
@@ -1,5 +1,6 @@
 package subway.utils
 
 enum class ErrorType(val errorMessage: String) {
-    INQUIRY_COMMAND(ERROR.format(ERROR_PATH_INQUIRY))
+    INQUIRY_COMMAND(ERROR.format(ERROR_PATH_INQUIRY)),
+    INQUIRY_TYPE_COMMAND(ERROR.format(ERROR_INQUIRY_TYPE))
 }
